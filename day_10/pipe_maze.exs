73f23f906d56e57bec65aacbe6269142b1486635
James Edward Gray II <james@payitoff.io>
2023-12-11 17:09:34 -0600

    Work in progress.

--- day_10/pipe_maze.exs
+++ day_10/pipe_maze.exs
@@ -2,39 +2,31 @@ defmodule PipeMaze do
   def read_input(path) do
     graph = :digraph.new()
 
-    path
-    |> File.stream!()
-    |> Stream.with_index()
-    |> Enum.each(fn {line, y} ->
-      line
-      |> String.trim()
-      |> String.graphemes()
-      |> Stream.with_index()
-      |> Enum.each(fn
-        {"|", x} ->
-          connect_pipes(graph, {x, y}, {x, y - 1}, {x, y + 1})
+    stream_nodes(path, fn {node, x, y} ->
+      :digraph.add_vertex(graph, {x, y}, node)
+    end)
 
-        {"-", x} ->
-          connect_pipes(graph, {x, y}, {x - 1, y}, {x + 1, y})
+    stream_nodes(path, fn
+      {"|", x, y} ->
+        connect_pipes(graph, {x, y}, {x, y - 1}, {x, y + 1})
 
-        {"L", x} ->
-          connect_pipes(graph, {x, y}, {x, y - 1}, {x + 1, y})
+      {"-", x, y} ->
+        connect_pipes(graph, {x, y}, {x - 1, y}, {x + 1, y})
 
-        {"J", x} ->
-          connect_pipes(graph, {x, y}, {x, y - 1}, {x - 1, y})
+      {"L", x, y} ->
+        connect_pipes(graph, {x, y}, {x, y - 1}, {x + 1, y})
 
-        {"7", x} ->
-          connect_pipes(graph, {x, y}, {x, y + 1}, {x - 1, y})
+      {"J", x, y} ->
+        connect_pipes(graph, {x, y}, {x, y - 1}, {x - 1, y})
 
-        {"F", x} ->
-          connect_pipes(graph, {x, y}, {x, y + 1}, {x + 1, y})
+      {"7", x, y} ->
+        connect_pipes(graph, {x, y}, {x, y + 1}, {x - 1, y})
 
-        {".", _x} ->
-          :ignore
+      {"F", x, y} ->
+        connect_pipes(graph, {x, y}, {x, y + 1}, {x + 1, y})
 
-        {"S", x} ->
-          :digraph.add_vertex(graph, {x, y}, "S")
-      end)
+      {"S", _x, _y} ->
+        :skip
     end)
 
     s =
@@ -68,15 +60,17 @@ defmodule PipeMaze do
 
     for x <- min_x..max_x, y <- min_y..max_y, {x, y} not in polygon, reduce: 0 do
       count ->
-        crossed_lines = Enum.count((x + 1)..max_x, fn x -> {x, y} in polygon end)
-        # crossed_lines =
-        #   Enum.count(1..max(max_x, max_y), fn offset ->
-        #     {x + offset, y + offset} in polygon
-        #   end)
-
-        # if rem(crossed_lines, 2) == 1 do
-        #   {x, y}
-        # end
+        crossed_lines =
+          1..max(max_x, max_y)
+          |> Enum.map(fn offset ->
+            case :digraph.vertex(graph, {x + offset, y + offset}) do
+              {_node, "S"} -> raise "unhandled error"
+              false -> 0
+              {_node, corner} when corner in ~w[L 7] -> 2
+              _node -> 1
+            end
+          end)
+          |> Enum.sum()
 
         if rem(crossed_lines, 2) == 1 do
           count + 1
@@ -86,13 +80,23 @@ defmodule PipeMaze do
     end
   end
 
-  defp connect_pipes(graph, from, to_1, to_2) do
-    Enum.each([from, to_1, to_2], fn v ->
-      unless :digraph.vertex(graph, v) do
-        :digraph.add_vertex(graph, v)
-      end
+  defp stream_nodes(path, f) do
+    path
+    |> File.stream!()
+    |> Stream.with_index()
+    |> Enum.each(fn {line, y} ->
+      line
+      |> String.trim()
+      |> String.graphemes()
+      |> Stream.with_index()
+      |> Enum.each(fn
+        {".", _x} -> :skip
+        {node, x} -> f.({node, x, y})
+      end)
     end)
+  end
 
+  defp connect_pipes(graph, from, to_1, to_2) do
     :digraph.add_edge(graph, {from, to_1}, from, to_1, [])
     :digraph.add_edge(graph, {from, to_2}, from, to_2, [])
   end
