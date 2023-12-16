99f25dee76ec5d2ae86be9404129ae508be8f90d
James Edward Gray II <james@graysoftinc.com>
2023-12-16 14:40:19 -0600

    Day 15, Part 2

--- day_15/lens_library.exs
+++ day_15/lens_library.exs
@@ -8,23 +8,61 @@ defmodule LensLibrary do
 
   def solve_part_1(steps) do
     steps
-    |> Enum.map(fn s ->
-      Stream.unfold({s, 0}, fn
-        {"", _hash} ->
-          nil
+    |> Enum.map(&hash/1)
+    |> Enum.sum()
+  end
 
-        {<<ascii::utf8, rest::binary>>, hash} ->
-          hash = rem((hash + ascii) * 17, 256)
-          {hash, {rest, hash}}
+  def solve_part_2(steps) do
+    steps
+    |> Enum.reduce(Map.new(), fn s, acc ->
+      case String.split(s, "=") do
+        [label, focal_length] ->
+          key = String.to_atom(label)
+          value = String.to_integer(focal_length)
+
+          Map.update(acc, hash(label), [{key, value}], fn hashmap ->
+            if Keyword.has_key?(hashmap, key) do
+              Keyword.replace!(hashmap, key, value)
+            else
+              [{key, value} | hashmap]
+            end
+          end)
+
+        _unmatched ->
+          label = String.slice(s, 0..-2)
+          key = String.to_atom(label)
+
+          Map.update(acc, hash(label), [], fn hashmap ->
+            Keyword.delete(hashmap, key)
+          end)
+      end
+    end)
+    |> Enum.flat_map(fn {box, lenses} ->
+      lenses
+      |> Enum.reverse()
+      |> Enum.with_index(1)
+      |> Enum.map(fn {{_label, focal_length}, slot} ->
+        (box + 1) * slot * focal_length
       end)
-      |> Enum.at(-1)
     end)
     |> Enum.sum()
+  end
+
+  defp hash(s) do
+    Stream.unfold({s, 0}, fn
+      {"", _hash} ->
+        nil
+
+      {<<ascii::utf8, rest::binary>>, hash} ->
+        hash = rem((hash + ascii) * 17, 256)
+        {hash, {rest, hash}}
+    end)
+    |> Enum.at(-1)
   end
 end
 
 System.argv()
 |> hd()
 |> LensLibrary.read_input()
-|> LensLibrary.solve_part_1()
+|> LensLibrary.solve_part_2()
 |> IO.inspect()
